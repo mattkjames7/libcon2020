6ee07dca049e9f1586f0669b57e91c5ff6d1a189
Matt <mattkjames7@gmail.com>
2022-11-04 22:29:18 +0000

    more batch fixes

--- src/compileobj.bat
+++ src/compileobj.bat
@@ -4,4 +4,5 @@ g++ -c -lm -fPIC -std=c++17 -Wextra bessel.cc -o ..\build\bessel.o
 g++ -c -lm -fPIC -std=c++17 -Wextra trap.cc -o ..\build\trap.o 
 g++ -c -lm -fPIC -std=c++17 -Wextra polyeval.cc -o ..\build\polyeval.o 
 g++ -c -lm -fPIC -std=c++17 -Wextra libcon2020.cc -o ..\build\libcon2020.o
+g++ -c -lm -fPIC -std=c++17 -Wextra smoothd.cc -o ..\build\smoothd.o
 
