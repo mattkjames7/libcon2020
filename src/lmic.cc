dce43e0ccd7b0d10966ea4b9f442c06f8dd0e9fc
Matt <mattkjames7@gmail.com>
2022-11-21 10:05:09 +0000

    added header file

--- src/lmic.cc
+++ src/lmic.cc
@@ -1,14 +1,58 @@
 #include "lmic.h"
 
 
-
+/*************************************************************
+*
+*	NAME: f_theta(thetai)
+*
+*	DESCRIPTION: Equation 5 of Cowley et al., 2008
+*
+*	INPUTS:
+*		double thetai	colatitude of the ionospheric footprint
+*						in radians!
+*
+*	RETURNS:
+*		double f_theta	1 + 0.25*tan^2 thetai
+*
+*************************************************************/
 doube f_thetai(double thetai) {
 
 	double tanti = tan(thetai);
 	return 1.0 + 0.25*tanti*tanti;
 }
 
-
+/*************************************************************
+*
+*	NAME: OmegaRatio(thetai,wO_open,wO_om,thetamm,dthetamm,
+*						thetaoc,dthetaoc)
+*
+*	DESCRIPTION: Ratio of the angular velocity mapped to
+*		thetai to the planetary rotation. Equation 15 of 
+*		Cowley et al., 2008.
+*
+*	INPUTS:
+*		double thetai	colatitude of the ionospheric footprint
+*						in radians!
+*		double wO_open	angular velocity ratio of open flux to
+*						planetary spin
+*		double wO_om	angular velocity ratio of outer magnetosphere
+*						to planetary spin
+*		double thetamm	ionospheric footprint latitude of the 
+*						middle magnetosphere boundary (where plasma 
+*						goes from rigid corotation to subcorotation)
+*						in radians.
+*		double dthetamm	width of the middle magnetosphere boundary
+*						in radians.
+*		double thetaom	ionospheric latitude of the open-closed field
+*						line boundary, in radians.
+*		double dthetaom	width of the open-closed field line boundary,
+*						in radians.
+*
+*	RETURNS:
+*		double wO		Ratio of plasma angular veloctiy to Jupiter
+*						spin.
+*
+*************************************************************/
 double OmegaRatio(	double thetai, double wO_open, double wO_om,
 					double thetamm, double dthetamm,
 					double thetaoc, double dthetaoc) {
@@ -21,6 +65,37 @@ double OmegaRatio(	double thetai, double wO_open, double wO_om,
 	return wO;
 }
 
+/*************************************************************
+*
+*	NAME: PedersenCurrent(thetai,g,wO_open,wO_om,thetamm,dthetamm,
+*						thetaoc,dthetsoc)
+*
+*	DESCRIPTION: Calculate the Pedersen current which maps to a
+*		given ionospheric latitude using equation 6 of Cowley et
+*		al., 2008.
+*
+*	INPUTS:
+*		double thetai	colatitude of the ionospheric footprint
+*						in radians!
+*		double g		dipole coefficient, nT.
+*		double wO_open	angular velocity ratio of open flux to
+*						planetary spin
+*		double wO_om	angular velocity ratio of outer magnetosphere
+*						to planetary spin
+*		double thetamm	ionospheric footprint latitude of the 
+*						middle magnetosphere boundary (where plasma 
+*						goes from rigid corotation to subcorotation)
+*						in radians.
+*		double dthetamm	width of the middle magnetosphere boundary
+*						in radians.
+*		double thetaom	ionospheric latitude of the open-closed field
+*						line boundary, in radians.
+*		double dthetaom	width of the open-closed field line boundary,
+*						in radians.
+*	RETURNS:
+*		double Ihp		Ionospheric Pedersen current.
+*
+*************************************************************/
 double PedersenCurrent(	double thetai, double g, 
 					double wO_open, double wO_om,
 					double thetamm, double dthetamm,
@@ -55,7 +130,32 @@ double PedersenCurrent(	double thetai, double g,
 	return IhP;
 }
 
-
+/*************************************************************
+*
+*	NAME: ThetaIonosphere(r,theta,g,r0,r1,mui2,D,deltarho,deltaz)
+*
+*	DESCRIPTION: Use the flux functions of the CAN model and a 
+*		dipole field to map the current position to a position
+*		on the ionosphere.
+*
+*	INPUTS:
+*		double r		radial coordinate, Rj.
+*		double theta	colatitude, radians.
+*		double g		dipole coefficient, nT.
+*		double r0		Inner edge of the current sheet, Rj.
+*		double r1		Outer edge of the current sheet, Rj.
+*		double mui2		current parameter, nT.
+*		double D		half-thickness of the current sheet, Rj.
+*		double deltarho	scale distance of the smoothing between
+*						inner and outer approximations, Rj.
+*		double deltaz	scale distance to smooth across the
+*						+/-D boundary, Rj.
+*
+*	RETURNS:
+*		double thetai	Ionospheric latitude in radians.
+*
+*
+*************************************************************/
 double ThetaIonosphere(	double r, double theta, double g,
 						double r0, double r1,
 						double mui2, double D, 
@@ -83,6 +183,46 @@ double ThetaIonosphere(	double r, double theta, double g,
 	return thetai;
 }
 
+/*************************************************************
+*
+*	NAME: BphiLMIC(r,theta,g,r0,r1,mui2,D,deltarho,deltaz,
+*					wO_open,wO_om,thetamm,dthetamm,
+*					thetaom,dthetaom)
+*
+*	DESCRIPTION: Calculate the azimuthal field using the LMIC 
+*		model.
+*
+*	INPUTS:
+*		double r		radial coordinate, Rj.
+*		double theta	colatitude, radians.
+*		double g		dipole coefficient, nT.
+*		double r0		Inner edge of the current sheet, Rj.
+*		double r1		Outer edge of the current sheet, Rj.
+*		double mui2		current parameter, nT.
+*		double D		half-thickness of the current sheet, Rj.
+*		double deltarho	scale distance of the smoothing between
+*						inner and outer approximations, Rj.
+*		double deltaz	scale distance to smooth across the
+*						+/-D boundary, Rj.
+*		double wO_open	angular velocity ratio of open flux to
+*						planetary spin
+*		double wO_om	angular velocity ratio of outer magnetosphere
+*						to planetary spin
+*		double thetamm	ionospheric footprint latitude of the 
+*						middle magnetosphere boundary (where plasma 
+*						goes from rigid corotation to subcorotation)
+*						in radians.
+*		double dthetamm	width of the middle magnetosphere boundary
+*						in radians.
+*		double thetaom	ionospheric latitude of the open-closed field
+*						line boundary, in radians.
+*		double dthetaom	width of the open-closed field line boundary,
+*						in radians.
+*
+*	RETURNS:
+*		double Bphi		Azimuthal field, nT.
+*
+*************************************************************/
 double BphiLMIC(double r, double theta, double g,
 						double r0, double r1,
 						double mui2, double D, 
