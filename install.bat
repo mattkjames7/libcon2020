1130c513e74e2a087f8777afde8417957be9dfb8
unknown <mattkjames7@gmail.com>
2022-11-22 17:17:14 +0000

    fixed batch files, compiles and runs successfully in Windows 10

--- install.bat
+++ install.bat
@@ -1,3 +1,3 @@
-cp -v .\lib\libcon2020\libcon2020.dll C:\TDM-GCC-64\lib
-cp -v .\include\con2020.h C:\TDM-GCC-64\include
-cp -v .\include\con2020c.h C:\TDM-GCC-64\include
+copy lib\libcon2020\libcon2020.dll C:\TDM-GCC-64\lib
+copy include\con2020.h C:\TDM-GCC-64\include
+copy include\con2020c.h C:\TDM-GCC-64\include
--- src/lmic.h
+++ src/lmic.h
@@ -4,6 +4,11 @@
 #include <stdlib.h>
 #include "flux.h"
 #include "sgn.h"
+#define _USE_MATH_DEFINES
+#include <math.h>
+#ifndef M_PI
+#define M_PI		3.14159265358979323846
+#endif
 
 extern "C" {
 	/*************************************************************
