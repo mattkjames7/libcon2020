bae04b584e31feda66c135f3f47310ad95cb89fd
matt <mattkjames7@gmail.com>
2024-08-09 00:26:10 +0100

    added more bessel tests

--- test/bessel.cc
+++ test/bessel.cc
@@ -33,7 +33,7 @@ TEST_CASE("Bessel function j0(x) test cases", "[j0(x)]") {
     };
     double result;
 
-    int i;
+    size_t i;
     for (i=0;i<x.size();i++) {
         result = j0(x[i]);
         REQUIRE(result == Approx(expected[i]).epsilon(1e-12));
@@ -55,13 +55,66 @@ TEST_CASE("Bessel function j0(n,x,j) test cases", "[j0(n,x,j)]") {
     std::vector<double> result(x.size());
 
     /* this bit will be simpler once I use vectors all around */
-    int n = (int) x.size();
+    size_t n = x.size();
     double xa[n], j[n];
-    int i;
+    size_t i;
     for (i=0;i<n;i++) {
         xa[i] = x[i]; 
     }
     j0(n,xa,j);
+    for (i=0;i<n;i++) {
+        result[i] = j[i]; 
+    }
+
+    for (i=0;i<x.size();i++) {
+        REQUIRE(compareVectors(result,expected));
+    }
+
+}
+
+
+TEST_CASE("Bessel function j1(x) test cases", "[j1(x)]") {
+
+    std::vector<double> x = {0.0,1.0,5.0,10.0,50.0,100.0};
+    std::vector<double> expected = {
+        0.0,
+        0.44005058574493355,
+        -0.3275791375914653,
+        0.04347274616886141,
+        -0.09751182812517509,
+        -0.0771453520141123,
+    };
+    double result;
+
+    size_t i;
+    for (i=0;i<x.size();i++) {
+        result = j1(x[i]);
+        REQUIRE(result == Approx(expected[i]).epsilon(1e-12));
+    }
+
+}
+
+TEST_CASE("Bessel function j1(n,x,j) test cases", "[j1(n,x,j)]") {
+
+    std::vector<double> x = {0.0,1.0,5.0,10.0,50.0,100.0};
+    std::vector<double> expected = {
+        0.0,
+        0.44005058574493355,
+        -0.3275791375914653,
+        0.04347274616886141,
+        -0.09751182812517509,
+        -0.0771453520141123,
+    };
+    std::vector<double> result(x.size());
+
+    /* this bit will be simpler once I use vectors all around */
+    size_t n = x.size();
+    double xa[n], j[n];
+    size_t i;
+    for (i=0;i<n;i++) {
+        xa[i] = x[i]; 
+    }
+    j1(n,xa,j);
     for (i=0;i<n;i++) {
         result[i] = j[i]; 
     }
